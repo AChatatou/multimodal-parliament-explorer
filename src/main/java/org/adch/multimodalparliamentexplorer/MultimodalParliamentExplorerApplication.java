28426cdd9cf23f7fd8916401c95a961ee1553c0d
AChatatou <adil.chtatou@gmail.com>
2026-04-19 12:25:39 +0200

    Initial commit

---
+++ src/main/java/org/adch/multimodalparliamentexplorer/MultimodalParliamentExplorerApplication.java
@@ -0,0 +1,13 @@
+package org.adch.multimodalparliamentexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimodalParliamentExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultimodalParliamentExplorerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/adch/multimodalparliamentexplorer/MultimodalParliamentExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adch.multimodalparliamentexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimodalParliamentExplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
